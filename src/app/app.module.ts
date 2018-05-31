fcb8055adc99f95893b0254b9b3a74701a95b0ea
rehlaender <avilamario16@gmail.com>
2018-05-31 14:18:41 -0700

    use store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './store/counter.reducer';
+
+import { CityComponentComponent } from './city-component/city-component.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
