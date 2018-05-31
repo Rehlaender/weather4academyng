dfa6b5fe1c9b6e80c94cd19333e9be84fed5780b
rehlaender <avilamario16@gmail.com>
2018-05-31 14:19:02 -0700

    use app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { INCREMENT, DECREMENT, RESET, SAYHI } from './store/counter.reducer';
+
+interface AppState {
+  count: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  count$: Observable<number>;
   title = 'app';
+  cities = [
+    { id: 1, name: "monterrey"}, {id: 2, name: "not monterrey" }
+  ]
+  constructor(private store: Store<AppState>) {
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
+  }
+
+  sayHi(city) {
+    this.store.dispatch({ type: SAYHI, payload: city });
+  }
 }
