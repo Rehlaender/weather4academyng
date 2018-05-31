7eeb34e825c450d1080bcc52731ed04a65bd815c
rehlaender <avilamario16@gmail.com>
2018-05-31 14:18:56 -0700

    add city component

---
+++ src/app/city-component/city-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'city-component',
+  templateUrl: './city-component.component.html',
+  styleUrls: ['./city-component.component.css']
+})
+export class CityComponentComponent implements OnInit {
+
+  @Output() sayHi: EventEmitter<any> = new EventEmitter();
+  @Input() city:any;
+
+  constructor() { }
+
+  dispatchSayHi(city) {
+     console.log(city, 'lel');
+     this.sayHi.emit(city);
+  }
+
+  ngOnInit() {
+  }
+
+}
