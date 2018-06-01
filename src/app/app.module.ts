c895d351deabd3f4fa019d758c54e6090be2f009
rehlaender <avilamario16@gmail.com>
2018-06-01 10:12:26 -0700

    use effects and actions in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './store/counter.reducer';
+import { weatherReducer } from './store/weather.reducers';
+
+import { EffectsModule } from '@ngrx/effects';
+import { WeatherEffects } from './effects/weather.effects';
 
 import { CityComponentComponent } from './city-component/city-component.component';
+import { CityformComponent } from './cityform/cityform.component';
+import { CityListComponent } from './city-list/city-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CityComponentComponent
+    CityComponentComponent,
+    CityformComponent,
+    CityListComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ count: counterReducer })
+    HttpClientModule,
+    FormsModule,
+    StoreModule.forRoot({ count: counterReducer, weather:weatherReducer }),
+    EffectsModule.forRoot([WeatherEffects])],
   ],
   providers: [],
   bootstrap: [AppComponent]
